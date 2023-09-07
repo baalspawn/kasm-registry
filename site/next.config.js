cc8ea7b5556402e50d9b4908f0a8d5a033c41367
baalspawn <olekpetersen@gmail.com>
2023-09-07 12:06:28 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,7 +2,7 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
+    name: 'Petersen Tech',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
     listUrl: 'https://baalspawn.github.io/kasm-registry/',
